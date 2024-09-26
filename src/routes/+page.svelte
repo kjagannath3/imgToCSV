9fa50c4ed18a6e89a19595d68c5b0c5d0798bf32
kjagannath3 <91837535+kjagannath3@users.noreply.github.com>
2024-09-25 22:31:35 -0400

    basic image container

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,20 @@
-<script></script>
+<script lang="ts">
+    let imageUrl: string | null = null;
+    let imageData: File | null = null;
+</script>
 
 <section>
-    <h1>Welcome to Image to CSV Converter</h1>
+    <h1 class="text-3xl">Welcome to Image to CSV Converter</h1>
     <p>Upload an image here to get CSV data</p>
-    <input type="file" id="imageInput" />
+    <input
+        type="file"
+        id="imageInput"
+        accept="image/*"
+    />
+    {#if imageUrl}
+
+        <img src={imageUrl} alt="Uploaded Image" id="imageUpload" width="200" />
+    {/if}
 
     <button>click to generate csv data</button>
 </section>
