cc7485d061758befe26dab242e5bfafc745cb252
kjagannath3 <91837535+kjagannath3@users.noreply.github.com>
2024-09-25 22:53:08 -0400

    file is able to be console.logged into the world

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,32 @@
 <script lang="ts">
     let imageUrl: string | null = null;
     let imageData: File | null = null;
+    /*function handleImageInput(event:Event) {
+        const input = event.target as HTMLInputElement
+        if (input.files && input.files.length > 0) {
+            const file = input.files[0]
+
+
+            if (file.type.startsWith('image')) {
+                console.log("Image selected", file)
+                const imagePreview = document.getElementById("imageUpload") as HTMLImageElement
+                imagePreview.src = URL.createObjectURL(file)
+
+            } else{
+                console.error("selected file is not an image");
+
+            }
+
+        }
+    }*/
+    function handleImageInput(event: Event) {
+        let selectedFile: File | null = null;
+        const input = event.target as HTMLInputElement;
+        if (input?.files && input.files.length > 0) {
+            selectedFile = input.files[0];
+            console.log(selectedFile);
+        }
+    }
 </script>
 
 <section>
@@ -10,11 +36,8 @@
         type="file"
         id="imageInput"
         accept="image/*"
+        on:change={handleImageInput}
     />
-    {#if imageUrl}
-
-        <img src={imageUrl} alt="Uploaded Image" id="imageUpload" width="200" />
-    {/if}
 
     <button>click to generate csv data</button>
 </section>
